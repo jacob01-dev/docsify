a7ce7cacf5506f949370003b8f279a00d9a542a0
JakubB <135221834+jakubB6@users.noreply.github.com>
2024-10-04 17:04:05 +0200

    fixed widget bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
       <body className={rubik.className}>
         {children}
         <Toaster />
