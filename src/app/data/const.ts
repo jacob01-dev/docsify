151bd2260bc252e61ec16056d530f17b8b270728
JakubB <135221834+jakubB6@users.noreply.github.com>
2024-10-04 17:02:31 +0200

    fixed widget bug

--- src/app/data/const.ts
+++ src/app/data/const.ts
@@ -1,2 +1,2 @@
 export const base_widget_url =
-  "https://cdn.jsdelivr.net/gh/jacob01-dev/Docsify-widget@main/widget.min.js";
+  "https://cdn.jsdelivr.net/gh/jacob01-dev/Docsify-widget/widget.min.js";
