e9d69f8780e9e467e039fec9e4334e3d12078fba
JakubB <135221834+jakubB6@users.noreply.github.com>
2024-10-06 15:29:35 +0200

    fixed cors errors

--- next.config.mjs
+++ next.config.mjs
@@ -8,16 +8,43 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
             key: "Access-Control-Allow-Origin",
-            value: "https://www.docsify.tech",
+            value: "https://docsify.tech", // No www
           },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
+            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
           },
           {
             key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
+          },
+        ],
+      },
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.docsify.tech", // With www
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
           },
         ],
       },
